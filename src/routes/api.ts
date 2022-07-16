bfeb48611aaf7d200cf2eed01b7835a91da1ae7b
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2022-07-15 18:51:19 -0700

    tried to fix path error for deployment

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,10 +1,9 @@
 const BASE_URL = `https://api.coinpaprika.com/v1`;
 
 export function fetchCoin() {
-    return fetch(`${BASE_URL}/coins`).then((response) =>
-        response.json()
-    );
+    return fetch(`${BASE_URL}/coins`).then((response) => response.json());
 }
+
 export function fetchCoinInfo(coinId: string) {
     return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
         response.json()
@@ -18,7 +17,7 @@ export function fetchCoinTickers(coinId: string) {
 }
 
 export function fetchCoinHistory(coinId: string) {
-    return fetch(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`).then((response) =>
-        response.json()
-    );
+    return fetch(
+        `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
+    ).then((response) => response.json());
 }
