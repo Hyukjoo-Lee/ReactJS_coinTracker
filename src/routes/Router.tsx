10bf333939b86c49fa18f1737f95efb1c592439e
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2022-07-15 19:29:40 -0700

    fixing router and api

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -4,9 +4,9 @@ import Coins from "./Coins";
 
 function Router() {
     return (
-        <BrowserRouter basename="/coinTracker_ReactJS">
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
-                <Route path="/:coinId/*" element={<Coin />}></Route>
+                <Route path="/:coinId" element={<Coin />}></Route>
                 <Route path="/" element={<Coins />}></Route>
             </Routes>
         </BrowserRouter>
